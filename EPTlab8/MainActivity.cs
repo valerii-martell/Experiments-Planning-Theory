f3be23a5ed82ab6d3b888016dba6d3f4a5d52c90
Valerii Martell <valerii.martell@gmail.com>
2018-05-13 20:00:50 +0200

    Add lab8

---
+++ EPTlab8/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using System;
+using System.Reflection;
+
+namespace EPTlab8
+{
+    [Activity(Label = "EPTlab8",
+    MainLauncher = true,
+    Icon = "@drawable/icon",
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+    ScreenOrientation = ScreenOrientation.Landscape)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            var buttonCompute = FindViewById<Button>(Resource.Id.buttonCompute);
+
+            buttonCompute.Click += (e, o) =>
+            {
+
+                //Matrix
+                var matrixTable = FindViewById<TableLayout>(
+                           Convert.ToInt32(
+                               typeof(Resource.Id).InvokeMember(
+                                   ("matrix1Table"), BindingFlags.GetField, null, null, null)));
+                matrixTable.Visibility = Android.Views.ViewStates.Visible;
+
+                matrixTable = FindViewById<TableLayout>(
+                           Convert.ToInt32(
+                               typeof(Resource.Id).InvokeMember(
+                                   ("matrix2Table"), BindingFlags.GetField, null, null, null)));
+                matrixTable.Visibility = Android.Views.ViewStates.Visible;
+
+                var tableView = FindViewById<TableLayout>(
+                           Convert.ToInt32(
+                               typeof(Resource.Id).InvokeMember(
+                                   ("equalTable"), BindingFlags.GetField, null, null, null)));
+                tableView.Visibility = Android.Views.ViewStates.Visible;
+            };
+        }
+    }
+}
+
